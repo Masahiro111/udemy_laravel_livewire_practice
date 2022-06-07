4312b65ba8ac5c3cbb1b7f2b162e7462fdebe19b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-07 19:02:06 +0900

    created 2-7

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,5 +2,7 @@
     <p>counter: {{ $counter }}</p>
     <p>
         <input wire:click="increment" type="button" value="+1">
+        <input wire:click="increment(10)" type="button" value="+10">
+        <input wire:click="$set('counter', 0)" type="button" value="set 0">
     </p>
 </div>
