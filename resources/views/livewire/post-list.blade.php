e018850649713aa9f433b72f89c78bc8b53a658c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-08 18:44:19 +0900

    finished 3-3

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,3 +1,9 @@
 <div>
-    おおぞらすばーう(=ﾟωﾟ)ﾉ
+    <ul>
+        @foreach ($posts as $post)
+        <li wire:key="post-{{ $post->id }}">
+            {{ $post->title }}
+        </li>
+        @endforeach
+    </ul>
 </div>
