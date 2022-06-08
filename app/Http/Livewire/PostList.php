6b3d5a77203ebe6fc9fe82d0040b26ec0790c94b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-08 19:37:34 +0900

    finished 3-16

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -8,15 +8,26 @@ use Livewire\Component;
 class PostList extends Component
 {
     public $posts;
-    // public $word;
+    public $word;
 
-    public function mount()
-    {
-        // $this->posts = Post::all();
-    }
+    // public function mount()
+    // {
+    //     $this->posts = Post::all();
+    // }
+
+    // public function updatedWord($value)
+    // {
+    //     $this->posts = Post::query()
+    //         ->where('title', 'LIKE', '%' . $value . '%')
+    //         ->get();
+    // }
 
     public function render()
     {
+        $this->posts = Post::query()
+            ->when($this->word, fn ($query, $value) => $query->where('title', 'LIKE', '%' . $value . '%'))
+            ->get();
+
         return view('livewire.post-list');
     }
 }
