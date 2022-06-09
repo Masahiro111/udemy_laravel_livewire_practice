e32d6130d9d41c077629f60784d66318f884d5c7
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-09 18:46:57 +0900

    3-24

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -10,53 +10,15 @@ class PostList extends Component
 {
     use WithPagination;
 
-    // public $title;
-    // public $body;
-    // public $post = [];
-
     public $word;
-    public Post $post;
-
 
     protected $queryString = [
         'word' => ['except' => ''],
     ];
 
-    protected $rules = [
-        'post.title' => ['required', 'max:8'],
-        'post.body' => ['required'],
-    ];
-
-    public function mount()
-    {
-        $this->post = new Post;
-    }
-
-    public function register()
-    {
-        $this->validate();
-
-        $this->post->save();
-
-        $this->post = new Post;
-
-
-        // $this->post->title = '';
-        // $this->post->title = '';
-        // Post::query()
-        //     ->create($data['post']);
-
-        // $this->reset('post');
-    }
-
     public function updatingWord()
     {
         $this->resetPage();
-    }
-
-    public function updated($key)
-    {
-        $this->validateOnly($key);
     }
 
     public function render()
