51f29de9b18be043e6e2be0355ed40b9b44f1419
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-13 18:45:52 +0900

    finished 3-41

--- resources/views/livewire/topic-item.blade.php
+++ resources/views/livewire/topic-item.blade.php
@@ -1,12 +1,10 @@
-<div>
-    <tr wire:key="topic-{{ $topic->id }}">
-        <td>{{ $topic->id }}</td>
-        <td>
-            {{ $topic->title }}
-        </td>
-        <td>
-            {{ Str::limit($topic->body, 30) }}
-        </td>
-        <td>変更する</td>
-    </tr>
-</div>
+<tr wire:key="topic-{{ $topic->id }}">
+    <td>{{ $topic->id }}</td>
+    <td>
+        {{ $topic->title }}
+    </td>
+    <td>
+        {{ Str::limit($topic->body, 30) }}
+    </td>
+    <td>変更する</td>
+</tr>
